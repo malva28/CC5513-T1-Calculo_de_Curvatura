f17c9b1ab76b79db87a930698141b3163be7dfba
malva28 <valentina.garrido@ug.uchile.cl>
2023-04-11 19:43:26 -0400

    Added script to calculate normals with openmesh

---
+++ normals.py
@@ -0,0 +1,64 @@
+import openmesh
+import numpy as np
+import polyscope as ps
+
+
+def calculate_face_normals(mesh: openmesh.TriMesh) -> np.ndarray:
+    face_normals = np.zeros((mesh.n_faces(), 3))
+    for face in mesh.faces():
+        v0, v1, v2 = [mesh.points()[v.idx()] for v in mesh.fv(face)]
+        normal = np.cross(v1 - v0, v2 - v0)
+        normal /= np.linalg.norm(normal)
+        face_normals[face.idx(), :] = normal
+
+    return face_normals
+
+
+def angle(center: np.ndarray, p1: np.ndarray, p2: np.ndarray) -> float:
+    vec_a = p1 - center
+    vec_b = p2 - center
+    norm_a = max(np.linalg.norm(vec_a), 1e-8)
+    norm_b = max(np.linalg.norm(vec_b), 1e-8)
+    theta = np.dot(vec_b, vec_a)/ (norm_a * norm_b)
+    return np.arccos(theta)
+
+
+def calculate_vertex_face_angles(mesh: openmesh.TriMesh) -> np.ndarray:
+    empty_dicts = np.frompyfunc(lambda x: {}, 1, 1)
+    vertex_angles = empty_dicts(np.zeros(mesh.n_vertices(), dtype=dict))
+    for face in mesh.faces():
+        p_vertices = [v for v in mesh.fv(face)]
+        vertices = [mesh.points()[v.idx()] for v in p_vertices]
+        for i in range(3):
+            i_prev = (i-1) % 3
+            i_next = (i+1) % 3
+            vert_angle = angle(vertices[i], vertices[i_prev], vertices[i_next])
+            vertex_angles[p_vertices[i].idx()][face.idx()] = vert_angle
+    return vertex_angles
+
+
+def calculate_vertex_normals_by_angle(mesh: openmesh.TriMesh) -> np.ndarray:
+    face_normals = calculate_face_normals(mesh)
+    vertex_face_angles = calculate_vertex_face_angles(mesh)
+
+    vertex_normals = np.zeros((mesh.n_vertices(), 3))
+
+    for vertex in mesh.vertices():
+        i_v = vertex.idx()
+        for face in mesh.vf(vertex):
+            vertex_normals[i_v,:] += face_normals[face.idx(), :] * vertex_face_angles[i_v][face.idx()]
+            vertex_normals[i_v,:] /= np.linalg.norm(vertex_normals[i_v,:])
+
+    return vertex_normals
+
+
+
+pth = "../../esfera/esfera1.off"
+mesh = openmesh.read_trimesh(pth)
+normales = calculate_face_normals(mesh)
+
+ps.init()
+ps_mesh = ps.register_surface_mesh("my mesh", mesh.points(), mesh.face_vertex_indices())
+ps.get_surface_mesh("my mesh").add_vector_quantity("face_normals", normales, defined_on='faces')
+ps.get_surface_mesh("my mesh").add_vector_quantity("vertex_normals", calculate_vertex_normals_by_angle(mesh))
+ps.show()
